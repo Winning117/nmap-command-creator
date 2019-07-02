52011523120208164584a5ad38fbe019b8e40d7b
Joshua Correia <joshua.correia@Dexcom.com>
2019-07-01 21:17:40 -0700

    Added bold text for headers

--- nmap_command_creator.py
+++ nmap_command_creator.py
@@ -1,11 +1,18 @@
 
 def main():
+
     ip = input("What IP are you trying to scan? ")
     print()
-    print("Full TDP Scan: nmap -p- -sV " + str(ip) + " > tdp_full_scan.txt\n")
-    print("Full UDP Scan: nmap -p- -sU -sV " + str(ip) + " > udp_full_scan.txt\n")
-    print("OS Scan: nmap -A " + str(ip) + " > os_information.txt\n")
-    print("Full services information: nmap -sV --version-intensity 5 " + str(ip) + " > full_services.txt\n")
+    print_section("Full TDP Scan: ", "nmap -p- -sV " + str(ip) + " > tdp_full_scan.txt\n")
+    print_section("Full UDP Scan: ", "nmap -p- -sU -sV " + str(ip) + " > udp_full_scan.txt\n")
+    print_section("OS Scan: ", "nmap -A " + str(ip) + " > os_information.txt\n")
+    print_section("Full services information: ", "nmap -sV --version-intensity 5 " + str(ip) + " > full_services.txt\n")
+
+
+def print_section(header, command):
+    BOLD = "\033[1m"
+    OFF = "\033[m"
+    print(BOLD + header + OFF + command)
 
 
 if __name__ == "__main__":
